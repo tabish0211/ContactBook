35cb9425b23e73a1215346b3eeefe0be92cbb60d
Tabish Ahmad <tabish0211@hotmail.com>
2023-09-04 10:51:43 +0530

    ado net

--- ContactBook/Controllers/UserController.cs
+++ ContactBook/Controllers/UserController.cs
@@ -28,11 +28,70 @@ namespace ContactBook.Controllers
 
             SqlConnection con = new SqlConnection("server=DESKTOP-UB4P45V;database=contact_db;integrated security=true;");
             con.Open();
-            string cmd= "insert into tblUser values(@username,@use"
-            SqlCommand cmd=new SqlCommand ()
+            string cmdStr = "insert into tblUser values(@username,@Password,@gender)";
+            SqlCommand cmd=new SqlCommand (cmdStr,con);
+            cmd.CommandType = CommandType.Text;
+
+            cmd.Parameters.AddWithValue("@username", user.UserName);
+            cmd.Parameters.AddWithValue("@Password", user.Password);
+
+            if (user.genderF.ToString()=="F")
+            {
+                cmd.Parameters.AddWithValue("@gender", user.genderF);
+            }
 
 
-            return Content("Record received successfully");
+            if (user.genderM.ToString()=="M")
+            {
+                cmd.Parameters.AddWithValue("@gender", user.genderM);
+            }
+
+            int rowSucceeded = cmd.ExecuteNonQuery();
+
+            cmd.Dispose();
+            con.Close();
+
+            User userDtl=FetchRecord(user);
+
+
+            return View("UserDetail",userDtl);
+        }
+
+        private User FetchRecord(User user)
+        {
+            SqlConnection con = new SqlConnection("server=DESKTOP-UB4P45V;database=contact_db;integrated security=true;");
+            con.Open();
+            string cmdStr = "select * from tblUser where username=@username";
+            SqlCommand cmd = new SqlCommand(cmdStr, con);
+            cmd.CommandType = CommandType.Text;
+
+            cmd.Parameters.AddWithValue("@username", user.UserName);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    user.UserName = dr["username"].ToString();
+                    user.Password = dr["password"].ToString();
+                    string gender = dr["gender"].ToString();
+                    if (gender=="F")
+                    {
+                        user.genderF = gender[0];
+                    }
+
+                    if (gender == "M")
+                    {
+                        user.genderM = gender[0];
+                    }
+                }
+            }
+
+            dr.Close();
+
+            return user;
+
         }
 
         public ActionResult Cancel()
